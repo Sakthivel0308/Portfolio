92c738883eb504acfe9ce6372e10ded5b219f4ed
Sakthivel <sakthivel.g0308@gmail.com>
2024-08-07 16:29:31 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Portfolio/",
+  plugins: [react()],
   css: {
     modules: {
-      localsConvention: 'camelCase'
-    }
-  }
-})
+      localsConvention: "camelCase",
+    },
+  },
+});
